ce678227f4004451bb68d3ef21b6c1a790cabd8a
Fredrik Dokka Nordstad <fredrikdn@gmail.com>
2019-10-11 09:21:15 +0200

    done - task 2

--- assignment3.py
+++ assignment3.py
@@ -4,20 +4,29 @@ import gensim
 import nltk
 import string
 from nltk.stem.porter import PorterStemmer
+from nltk.probability import FreqDist
+import re
 
 
-file = "/Users/Fredrik/Documents/Skole/H19/TDT4117/TDT4117_Assignmet3/pg3300.txt"
+tex_file = "/Users/Fredrik/Documents/Skole/H19/TDT4117/TDT4117_Assignmet3/pg3300.txt"
 solution = "/Users/Fredrik/Documents/Skole/H19/TDT4117/TDT4117_Assignmet3/solutions.txt"
+
+# #### Task 1 ####
 
 # Task 1.0
 stemmer = PorterStemmer()
 random.seed(123)
+fdist = FreqDist()
 
 # Task 1.1
+
+
 def openFile(file):
     return codecs.open(file, "r", "utf-8")
 
 # Task 1.2
+
+
 def paragraphSplitter(file):
     fil = openFile(file)
     paragraphs = []
@@ -32,6 +41,8 @@ def paragraphSplitter(file):
     return paragraphs
 
 # Task 1.3
+
+
 def wordRemover(paragraphs, word):
     paraList = []
     for p in paragraphs:
@@ -40,12 +51,16 @@ def wordRemover(paragraphs, word):
     return paraList
 
 # Task 1.4
+
+
 def tokenizeParagraphs(paragraphs):
     for i, p in enumerate(paragraphs):
         paragraphs[i] = p.split(" ")
     return paragraphs
 
 # Task 1.5
+
+
 def lower(paragraphs):
     paras_lowered = []
     for p in paragraphs:
@@ -55,44 +70,122 @@ def lower(paragraphs):
         paras_lowered.append(word_lowered)
     return paras_lowered
 
+
 def removePunctuation(paragraphs):
     paras = []
+    #goes through each word in the paragraph and removes whitespace, etc.
     for p in paragraphs:
         words = []
         for word in p:
-            temp = word.replace('\r\n', ' ')
+            temp = replaceMultiple(word, ['.',',',';',':','\r\n'], ' ')
             templist = temp.split(' ')
             for i in templist:
-                words.append(i)
+                if i == '':
+                    continue
+                else:
+                    words.append(i)
         paras.append(words)
     return paras
 
+# aiding function to remove multiple chars
 
 
-def getParagraphs(paragraphs):
-    for i, words in enumerate(paragraphs):
-        words = removePunctuation(words)
-        paragraphs[i] = words
-    return paragraphs
+def replaceMultiple(w, toBeRemoved, r):
+    for elem in toBeRemoved:
+        if elem in w:
+            w = w.replace(elem, r)
+    return w
 
 # Task 1.6
-def stemWords(paragraphs):
-    stemmedP = []
+
+
+def stem(paragraphs):
+    stemmed_paras = []
+    #for each paragraph, stem each word in the paragraph
     for p in paragraphs:
-        stemmedW = []
-        for w in p:
-            stemmed.append(stemmer.stem(w).lower())
-    return paragraphs
+        stemmed_words = []
+        for word in p:
+            stemmed_words.append(stemmer.stem(word).lower())
+        stemmed_paras.append(stemmed_words)
+    return stemmed_paras
 
 # Task 1.7
 
-# Task 1 - solutions
 
-textFile = paragraphSplitter(file)
-paragraphList = wordRemover(textFile, "Gutenberg")
-paragraphList = tokenizeParagraphs(paragraphList)
-paragraphList = lower(paragraphList)
-paragraphList = removePunctuation(paragraphList)
+def wordFrequency(paragraphs, word):
+    #searching the word in question
+    #word = stemmer.stem(word)
+    for p in paragraphs:
+        for w in p:
+            if w == word:
+                fdist[w] += 1
+    return fdist
 
-print(paragraphList[1200])
 
+# Task 1 - run
+paragraphs = paragraphSplitter(tex_file)
+paragraphList = wordRemover(paragraphs, "Gutenberg")
+tokenizedParas = tokenizeParagraphs(paragraphList)
+paragraphList = lower(tokenizedParas)
+remParas = removePunctuation(paragraphList)
+paragraphList = stem(remParas)
+
+#print(paragraphList[1800])
+c = wordFrequency(remParas, "consumer")
+print(c)
+
+# #### Task 2 ####
+
+# creating the dictionary with the edited list of paragraphs
+dictionary = gensim.corpora.Dictionary(paragraphList)
+print(dictionary)
+
+# stopwords_file
+stopwords_file = "/Users/Fredrik/Documents/Skole/H19/TDT4117/TDT4117_Assignmet3/common-english-words.txt"
+
+# creating a list of stopwords from the attached file:
+
+
+def stopwordList(stopwords_file):
+    stopwordList = []
+    fil = openFile(stopwords_file)
+    stopwords_read = fil.read()
+    stopwordList = stopwords_read.split(',')
+    return stopwordList
+
+# finding the ids for the stopwords in the dictionary:
+
+
+def stopword_id(stopwords, dictionary):
+    ids = []
+    for word in stopwords:
+        try:
+            ids.append(dictionary.tokend2id[word])
+        except:
+            pass
+    return ids
+
+# converting the paragraphlist to bag-of-words:
+
+
+def convert_bow(paragraphs):
+    for p in paragraphs:
+        bags.append(dictionary.doc2bow(p))
+
+
+# bag of words
+bags = []
+
+# the stopword-list
+stopwords = stopwordList(stopwords_file)
+print(stopwords)
+
+# getting the list of stopword IDs
+stopword_ids = stopword_id(stopwords, dictionary)
+
+# filter out stopwords from the dictionary
+dictionary.filter_tokens(stopword_ids)
+
+# converted to BoW
+b_o_w = convert_bow(paragraphList)
+print(bags)
