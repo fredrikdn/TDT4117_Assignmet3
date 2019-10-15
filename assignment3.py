779fcceefa03c98e722e7573c6da4f43153fef86
Fredrik Dokka Nordstad <fredrikdn@gmail.com>
2019-10-15 17:23:33 +0200

    All tasks prior to TASK 4.3 should work fine, can't print the 5 first lines for task 4.3

--- assignment3.py
+++ assignment3.py
@@ -3,6 +3,7 @@ import codecs
 import gensim
 import nltk
 import string
+from itertools import groupby
 from nltk.stem.porter import PorterStemmer
 from nltk.probability import FreqDist
 
@@ -22,6 +23,7 @@ fdist = FreqDist()
 
 def open_file(file):
     return codecs.open(file, "r", "utf-8")
+
 
 # Task 1.2
 
@@ -75,22 +77,35 @@ def lower(paragraphs):
         paras_lowered.append(word_lowered)
     return paras_lowered
 
+# remove punctuation of all words in the corpus
+
 
 def remove_punctuation(paragraphs):
     paras = []
     # goes through each word in the paragraph and removes whitespace, etc.
     for p in paragraphs:
-        words = []
-        for word in p:
-            temp = replace_multiple(word, ['.', ',', ';', ':', '\r\n'], ' ')
-            templist = temp.split(' ')
-            for i in templist:
-                if i == '':
-                    continue
-                else:
-                    words.append(i)
+        words = remove_punctuation_word(p)
         paras.append(words)
     return paras
+
+# aiding function to remove the punctuation of each paragraph
+
+punc = list(string.punctuation)
+punc.append('\r\n')
+
+
+def remove_punctuation_word(paragraph):
+    words = []
+    for word in paragraph:
+        temp = replace_multiple(word, punc, ' ')
+        templist = temp.split(' ')
+        for i in templist:
+            if i == '':
+                continue
+            else:
+                words.append(i)
+    return words
+
 
 # aiding function to remove multiple chars
 
@@ -103,16 +118,25 @@ def replace_multiple(w, toBeRemoved, r):
 
 # Task 1.6
 
+# stem paragraphs
+
 
 def stem(paragraphs):
     stemmed_paras = []
     # for each paragraph, stem each word in the paragraph
     for p in paragraphs:
-        stemmed_words = []
-        for word in p:
-            stemmed_words.append(stemmer.stem(word).lower())
+        stemmed_words = stem_word(p)
         stemmed_paras.append(stemmed_words)
     return stemmed_paras
+
+# stem each word in paragraph
+
+
+def stem_word(p):
+    stemmed_words = []
+    for word in p:
+        stemmed_words.append(stemmer.stem(word).lower())
+    return stemmed_words
 
 # Task 1.7
 
@@ -128,81 +152,63 @@ def word_frequency(paragraphs, word):
 
 
 # Task 1 - structure and edit corpus
-paragraphs = paragraph_splitter(tex_file)
-noGuten = word_remover(paragraphs, "Gutenberg")
+split = paragraph_splitter(tex_file)
+noGuten = word_remover(split, "Gutenberg")
+print("aaa{}".format(noGuten[12]))
 tokenizedParas = tokenize_paragraphs(noGuten)
 lowered = lower(tokenizedParas)
 remParas = remove_punctuation(lowered)
 stemParas = stem(remParas)
 
 
-# runs the word_frequency-function on the documents before stemming
-c = word_frequency(remParas, "consumer")
-print("Word Frequency: {}".format(c))
-
-# runs the word_frequency-function on the documents after stemming
-d = word_frequency(stemParas, "consumer")
-print("Word Frequency: {}".format(d))
-
-# ==> works both before and after stemming
+# runs the word_frequency-function with the given word on the documents
+w = "consumer"
+c = word_frequency(remParas, w)
+print("Word Frequency ({}): {}".format(w,c))
 
 
 # #### TASK 2 ####
 
 
-# creating the dictionary with the edited list of paragraphs
+# stop words extracted from the common-english-words.txt
+stopwords = 'a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,' \
+             'cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,' \
+             'how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,' \
+             'not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,' \
+             'their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,' \
+             'who,whom,why,will,with,would,yet,you,your'
+
+stop_word_list = stopwords.split(',')
+
+# Building a dictionary based on the corpus from Task 1
 dictionary = gensim.corpora.Dictionary(stemParas)
-#print(dictionary)
 
-# stopwords_file
-stopwords_file = "/Users/Fredrik/Documents/Skole/H19/TDT4117/TDT4117_Assignmet3/common-english-words.txt"
-
-# creating a list of stopwords from the attached file:
-
-
-def stopword_list(stopwords_file):
-    stopwordList = []
-    fil = open_file(stopwords_file)
-    stopwords_read = fil.read()
-    stopwordList = stopwords_read.split(',')
-    return stopwordList
-
-# finding the ids for the stopwords in the dictionary:
-
-
-def stopword_id(stopwords, dictionary):
+# finds the ids for the stopwords from the dictionary
+def stop_word_ids(stopwords, dictionary):
     ids = []
     for word in stopwords:
         try:
-            ids.append(dictionary.tokend2id[word])
+            ids.append(dictionary.token2id[word])
         except:
             pass
     return ids
 
-# converting the paragraphlist(corpus) to bag-of-words:
 
+# list of the ids
+stopword_ids = stop_word_ids(stop_word_list, dictionary)
 
-def convert_bow(paragraphs):
-    for p in paragraphs:
-        bags.append(dictionary.doc2bow(p))
-
-
-# bag of words
-bags = []
-
-# the stopword-list
-stopwords = stopword_list(stopwords_file)
-#print(stopwords)
-
-# getting the list of stopword IDs
-stopword_ids = stopword_id(stopwords, dictionary)
-
-# filter out stopwords from the dictionary
+# filter out the "bad ids" from the dictionary
 dictionary.filter_tokens(stopword_ids)
 
-# converted to BoW
-convert_bow(stemParas)
-print(bags)
+# runs through each document(paragraph) of the corpus and adds the given words to the BoW
+def to_bow(paragraphs):
+    bow = []
+    for p in paragraphs:
+        bow.append(dictionary.doc2bow(p))
+    return bow
+
+bow = to_bow(stemParas)
+#print(bow)
 
 
 # #### TASK 3 ####
@@ -210,10 +216,10 @@ print(bags)
 
 # Task 3.1, 3.2, 3.3
 # the TF-IDF model based on BoW
-tfidf_model = gensim.models.TfidfModel(bags)
+tfidf_model = gensim.models.TfidfModel(bow)
 
 # the BoW on the following format for each word: (index, weight)
-tfidf_corpus = tfidf_model[bags]
+tfidf_corpus = tfidf_model[bow]
 
 # matrix similarity of the corpus
 matrix_sim = gensim.similarities.MatrixSimilarity(tfidf_corpus)
@@ -221,7 +227,7 @@ matrix_sim = gensim.similarities.MatrixSimilarity(tfidf_corpus)
 # Task 3.4
 # this will group documents and words, based on their tf-idf-weight, together into topics (serializing)
 lsi_model = gensim.models.LsiModel(tfidf_corpus, id2word=dictionary, num_topics=100)
-lsi_corpus = lsi_model[bags]
+lsi_corpus = lsi_model[bow]
 lsi_matrix = gensim.similarities.MatrixSimilarity(lsi_corpus)
 
 # Task 3.5
@@ -229,25 +235,70 @@ lsi_matrix = gensim.similarities.MatrixSimilarity(lsi_corpus)
 topic1 = lsi_model.show_topic(1)
 topic2 = lsi_model.show_topic(2)
 topic3 = lsi_model.show_topic(3)
+print("\n\nTOPICS: ", "\n")
 print(topic1)
-print("\n")
 print(topic2)
-print("\n")
-print(topic3)
+print(topic3, "\n")
 
-# The three topics are related in the way that they are all about economy
-# one possible division between the three can be:
-#   - topic1: content about the workforce and production value/cost
-#   - topic2: content about the specific produce(what is produced), the quantity and value/price
-#   - topic3: content about trade/tax and foreign affairs
-# Each topic includes the most important words in defining the topic are included
-# in the output, along with their contribution to the topic
+print("The three topics are related in the way that they are structured such that\n"
+      "each cluster of docs and similar words in the occurence matrix represents the topics."
+      "\nEach topic includes the most important words in defining the topic in the output, "
+      "\nalong with their contribution to the topic.\n")
 
 
 # #### TASK 4 ####
 
+# pre-processing of the query
+
+def preprocessing(query):
+    query = query.lower()
+    query = query.split()
+    query = remove_punctuation_word(query)
+    query = stem_word(query)
+    return query
+
 # Task 4.1
+#preprocessing of the query
+query = "How taxes influence Economics?"
+query = preprocessing(query)
+query = dictionary.doc2bow(query)
+print(query)
 
-queries = []
+# Task 4.2
+# BoW to TF-IDF
+tfidf_index = tfidf_model[query]
 
+# This function will structure the output (tf-idf) the way it is described in the assignment
+def bow_to_tfidf(index, dictionary):
+    list=[]
+    for word in index:
+        word_index = word[0]
+        word_weight = word[1]
+        str = "index: {} , word: {} , weight: {}".format(word_index, dictionary.get(word_index, word_weight), word_weight)
+        list.append(str)
+    return list
+
+
+tf_form = bow_to_tfidf(tfidf_index, dictionary)
+print("TF-IDF representation of the query: \n{}\n\n".format(tf_form))
+
+# Task 4.3
+
+# helper to split into readable paragraphs (list of all docs)
+
+
+docsim = enumerate(matrix_sim[tfidf_index])
+# sorting the docs
+top_res = sorted(docsim, key=lambda x: x[1], reverse=True)[:3]
+
+
+# return the top 3 most relevant docs
+for res in top_res:
+    doc = noGuten[res[0]]
+    print("\n[Document: {}]".format(res[0]))
+    for line in range(5):
+        try:
+            print(doc[line])
+        except:
+            pass
 
